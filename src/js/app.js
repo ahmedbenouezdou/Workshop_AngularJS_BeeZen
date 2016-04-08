132b4f93fe82c8ad2f515bfa7ff221cc5a344c92
Ahmed Ben Ouezdou <ahmed.ben-ouezdou@ggv.fr>
2016-04-08 16:04:53 +0200

    step-7 $routeProvider

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ['ngRoute', 'ui.bootstrap']);
 
 app.constant("myConfig", {
     "url": "http://localhost",
@@ -6,17 +6,17 @@ app.constant("myConfig", {
 });
 
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
 
         when('/', {
-            templateUrl: './view/listTask.htm',
+            templateUrl: './src/view/listTask.html',
             controller: 'taskCrt'
         }).
 
         when('/add', {
-            templateUrl: './view/addTask.htm',
-            controller: 'addTaskCrt'
+            templateUrl: './src/view/addTask.html',
+            controller: 'taskCrt'
         }).
 
         otherwise({
@@ -26,68 +26,142 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 
-app.controller("taskCrt", function ($scope, taskService) {
+app.controller("taskCrt", function ($scope, taskService, $window) {
 
     function init() {
-        taskService.getAll().success(function (data) {
-            $scope.listTasks = data;
-        });
+        $scope.listTasks = taskService.getAll();
         $scope.titleTask = "liste de tache";
 
         $scope.newTask = "";
     }
 
     $scope.addTask = function addTask() {
-        if ($scope.newTask !== "") {
-            var dataInsert = dataTask($scope.newTask);
-            taskService.addTask(index).success(function () {
-                $scope.listTasks.splice(dataInsert);
-            });
-        }
-
-        $scope.newTask = "";
-
+        $scope.listTasks = taskService.addTask(dataTask($scope.newTask));
+        $window.location.href = '#/';
     }
 
     $scope.deleteTask = function deleteTask(index) {
-
-        taskService.deleteTask(index).success(function () {
-            $scope.listTasks.splice(index, 1);
-        });
+        $scope.listTasks.splice(index, 1) ;
 
     }
 
     function dataTask(newTask) {
-        return {id: $scope.listTasks.length, titre: newTask, dateProgrom: "2016-03-02"};
+        return {id: $scope.listTasks.length, titre: newTask.titre, dateProgrom: newTask.date};
     }
 
     init();
+
+    $scope.today = function () {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    $scope.inlineOptions = {
+        customClass: getDayClass,
+        minDate: new Date(),
+        showWeeks: true
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        maxDate: new Date(2020, 5, 22),
+        minDate: new Date(),
+        startingDay: 1
+    };
+
+    // Disable weekend selection
+    function disabled(data) {
+        var date = data.date,
+            mode = data.mode;
+        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    }
+
+    $scope.toggleMin = function () {
+        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+    };
+
+    $scope.toggleMin();
+
+    $scope.open1 = function () {
+        $scope.popup1.opened = true;
+    };
+
+    $scope.open2 = function () {
+        $scope.popup2.opened = true;
+    };
+
+    $scope.setDate = function (year, month, day) {
+        $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'dd/MM/yyyy', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[1];
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+    $scope.popup1 = {
+        opened: false
+    };
+
+    $scope.popup2 = {
+        opened: false
+    };
+
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date(tomorrow);
+    afterTomorrow.setDate(tomorrow.getDate() + 1);
+    $scope.events = [
+        {
+            date: tomorrow,
+            status: 'full'
+        },
+        {
+            date: afterTomorrow,
+            status: 'partially'
+        }
+    ];
+
+    function getDayClass(data) {
+        var date = data.date,
+            mode = data.mode;
+        if (mode === 'day') {
+            var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+            for (var i = 0; i < $scope.events.length; i++) {
+                var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                if (dayToCheck === currentDay) {
+                    return $scope.events[i].status;
+                }
+            }
+        }
+
+        return '';
+    }
 
 });
 
 
 app.service("taskService", function ($http, myConfig) {
 
+    var listTaskService = [
+        {id: 1, titre: "Task1", dateProgrom: "2016-03-02"},
+        {id: 2, titre: "Task2", dateProgrom: "2016-03-02"}
+    ]
+
     this.getAll = function getAll() {
-        return $http.get(myConfig.url + ":" + myConfig.port + "/serverGetAll");
+        return listTaskService;
     }
 
     this.addTask = function addTask(data) {
 
-       return $http({
-            method: "post",
-            url: myConfig.url + ":" + myConfig.port + "/serverPostAdd",
-            data: data
-        });
+        return listTaskService.push(data);
 
 
-    }
-
-    this.deleteTask = function deleteTask(index) {
-        return $http({
-            method: "delete",
-            url: myConfig.url + ":" + myConfig.port + "/serverDelete",
-            data: {index:index}
-        });
     }
 });
